4fef7a4df417dd9cc048a29c6c37a927c4944f4d
Nam Wook Kim <namwkim85@gmail.com>
2016-12-12 23:35:02 -0500

    bubbleview site

--- namapp/__init__.py
+++ namapp/__init__.py
@@ -19,7 +19,10 @@ datasets = [ "all5k", "single2k", "targets410", "targets393"]
 @app.route('/')
 def index():
     return render_template("index.html")
-
+    
+@app.route('/bubbleview')
+def index():
+    return render_template("bubbleview.html")
 # if __name__ == '__main__':
 #     app.run(debug=True, host='0.0.0.0', port='6353') # app.debug = True  : this will reload server on code changes
 
